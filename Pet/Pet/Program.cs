9e09f97619204833fea066c1429a0dedeb0437bb
opilane <opilane@HP-CZC0359NN5>
2021-11-30 15:50:42 +0200

    first commit

---
+++ Pet/Pet/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace Pet
+{
+    class Program
+    {
+       
+        class Pet
+        {
+            string name;
+            int age;
+            double weight;
+
+            public Pet(string _name)
+            {
+                name = _name;
+                age = 0;
+                weight = 0.1;
+                Console.WriteLine($"Congratulatsion! You've adopted {name}.");
+            }
+
+            //getter for weight
+            public double Weight
+            {
+                get { return weight; }
+            }
+
+            public void ShowPetinfo()
+            {
+                Console.WriteLine($"Name: {name}.");
+                Console.WriteLine($"Age: {age}.");
+                Console.WriteLine($"Weight: {weight}.");
+            }
+
+            public void Eat()
+            {
+                weight += 0.2;
+                Console.WriteLine("omnomon");
+            }
+
+
+
+            public void Run()
+            {
+                weight -= 0.1;
+                Console.WriteLine("spin-spin-spin");
+            }
+
+            //desctructor
+
+            //~Pet()
+            //{
+            //    Console.WriteLine($"RIP {name}.");
+            //}
+
+
+        }
+        static void Main(string[] args)
+        {
+            Pet myPet = new Pet("Hamster");
+            myPet.ShowPetinfo();
+
+            for(int i =0; i < 10; i++)
+            {
+                myPet.Eat();
+                Console.WriteLine($"Current weight: {myPet.Weight}");
+                if(myPet.Weight >=0.7)
+                {
+                    while (myPet.Weight > 0.7)
+                    {
+                        myPet.Run();
+                        Console.WriteLine($"Weight after workout: {myPet.Weight}");
+                    }
+                }
+            }
+
+
+            
+            //GC.Collect();
+        }
+    }
+}
